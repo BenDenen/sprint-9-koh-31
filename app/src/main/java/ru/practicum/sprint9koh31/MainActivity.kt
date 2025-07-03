7c6aa4940e48944c7c10bd14a92f95615ee80a67
Boris Denisenko <Shumer512@gmail.com>
2025-07-03 19:26:48 +0200

    Webinar sprint 9

--- app/src/main/java/ru/practicum/sprint9koh31/MainActivity.kt
+++ app/src/main/java/ru/practicum/sprint9koh31/MainActivity.kt
@@ -1,20 +1,78 @@
 package ru.practicum.sprint9koh31
 
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var isProcessing = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+//        editText.addTextChangedListener(object : TextWatcher{
+//            override fun afterTextChanged(s: Editable) {
+//                Log.d("MyTag", "afterTextChanged $s")
+//                if(!isProcessing) {
+//                    isProcessing = true
+//                    s.append("$")
+//                    isProcessing = false
+//                }
+//
+//
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                Log.d("MyTag", "onTextChanged $s")
+//
+//            }
+//
+//        })
+
+        editText.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val text = savedInstanceState.getString("editText") ?: "Empty"
+    }
+
+    companion object {
+        const val TAG="MainActivity"
     }
 }
